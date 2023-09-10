9abf684f10a4bc780352a8362d994509ac7e6207
EGORxGG <legoegorgelen@gmail.com>
2023-09-10 14:53:54 +0200

    Add code with debug lines

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,1 +1,31 @@
+#include <EasyHID.h>
 
+void setup() {
+  HID.begin();
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  keyboardF1();
+  connectedBlink();
+  HID.tick();
+}
+
+void keyboardF1() {
+  static uint32_t timer;
+  if (millis() - timer >= 5000) {
+    Serial.println("keyF1");
+    timer = millis();
+    Keyboard.click(KEY_F1);
+  }
+}
+
+void connectedBlink() {
+  static uint32_t timer;
+  if (millis() - timer >= 1000) {
+    Serial.println("blink");
+    timer = millis();
+    digitalWrite(13, !digitalRead(13));
+  }
+}
