7417dc47887fa876bd8fbe12de1596f4fe905047
EGORxGG <legoegorgelen@gmail.com>
2023-09-10 18:23:30 +0200

    Move usb init back into setup, Add startup delay

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,22 +1,24 @@
 #include <EasyHID.h>
 
 void setup() {
+  delay(30000);
   Serial.begin(9600);
   pinMode(13, OUTPUT);
+  HID.begin();
 }
 
 void loop() {
   keyboardF1();
   connectedBlink();
+  HID.tick();
 }
 
 void keyboardF1() {
   static uint32_t timer;
   if (HID.isConnected() && millis() - timer >= 5000) {
-    HID.begin();
+    timer = millis();
     Serial.println("keyF1");
     Keyboard.click(KEY_F1);
-    HID.end();
   }
 }
 
